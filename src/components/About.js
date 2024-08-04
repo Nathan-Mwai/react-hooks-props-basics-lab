49bdcf070324c00e64c444adb6b0fb7b16a34d6c
Nathan Mwai <nathan.mwai@student.moringaschool.com>
2024-08-04 16:57:46 +0300

    .trim removes spaces from string

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,22 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function About(props) {
+  
+  function renderBio() {
+    if (props.bio && props.bio.trim() !== "") {
+      return <p>{props.bio}</p>;
+    }
+
+    return null;
+  }
+
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {renderBio()}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github={props.github} linkedin={props.linkedin} />
     </div>
   );
 }
--- src/components/App.js
+++ src/components/App.js
@@ -11,8 +11,8 @@ function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home color={user.color} name={user.name} city={user.city} bio={user.bio} />
+      <About bio={user.bio} github={user.links.github} linkedin={user.links.linkedin}/>
     </div>
   );
 }
